61305fee9e5bdd5d26ffbdac0132fb781fc104ff
DustySoul1 <mukilan.2.2014@gmail.com>
2024-12-31 15:35:39 -0600

    Instant Installation prompt

---
+++ install.js
@@ -0,0 +1,10 @@
+// install.js
+
+let installPrompt = null;
+const installButton = document.querySelector("#install");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installPrompt = event;
+  installButton.removeAttribute("hidden");
+});
